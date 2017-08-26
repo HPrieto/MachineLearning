6deabcb5f3c5e08998861001c053b03ffed045e7
Heriberto Prieto <duskayame@gmail.com>
2017-08-26 05:24:18 -0700

    vectorized logistic regression

---
+++ Coursera/intro.py
@@ -0,0 +1,23 @@
+print("Video 1:")
+print("E: Experience")
+print("T: Task")
+print("P: Performance \n\n\n")
+
+
+print("Video 2:")
+print("Supervised: Right answers were given")
+print("  - Dataset was given with some answers given")
+print("  - Learned about new datasets based on given data\n\n")
+
+print("Regression: Predict continuous value output.\n\n")
+
+print("Classification: Discrete valued output (0, 1)")
+print("   - Can classify more than two types\n\n\n\n")
+
+
+print("Unsupervised Learning: Automatically find structure in unknown data.")
+print("Clustering Types: ")
+print(" - Database computing clusters")
+print(" - Social Networking Sites (who knows who)")
+print(" - Market Segmentation: Targeting similar customers more effectively")
+print(" - Astronomical Data Analysis: Gives useful theories on how galaxies are formed.\n\n")
---
+++ Coursera/regression.py
@@ -0,0 +1,57 @@
+print("Notation:\n")
+
+print("m = Number of training examples")
+print("(theta)i's = parameters")
+print("x's = 'input' variable/features")
+print("y's = 'output' variable/target variable")
+print("(x, y) = one training example")
+print("(xi, yi) = ith training example\n\n")
+
+print("Univariate Linear Regression Steps:")
+print("1.) Training Set: Data")
+print("        ||")
+print("        \/")
+print("2.) Learning Algorithm: Outputs 'h'(function that maps from x's to y's)")
+print("        ||")
+print("        \/")
+print("3.) 'h'(hypothesis) takes in x and tries to output some y(Estimated Price)")
+
+print("Cost Function: Allows us how to figure how to fit the best possible line to our data.\n\n\n")
+
+print("Hypothesis: h (theta)(x) = (theta)0 + (theta)1(x)")
+print("Cost Function: ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
