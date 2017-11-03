3e321695114eda51ecf1790d8820f4b3ab4edbfd
Heriberto Prieto <duskayame@gmail.com>
2017-11-02 23:28:32 -0700

    numerical computation softmax implementation

--- numerical_computation.py
+++ numerical_computation.py
@@ -1,11 +1,5 @@
 import numpy as np
 
-X = np.random.randn(100)
-
-O = np.ones(500)*5
-
-print(X)
-
 def softmax(x,X):
     """
         x: single input scalar from training set
@@ -13,8 +7,25 @@ def softmax(x,X):
     """
     return np.exp(x) / np.sum(np.exp(X))
 
-print('\n\n\n',softmax(X[2],X))
+# some constant value
+v = 100
+# very negative array
+N = np.ones(500) * -v
+print(N)
+# exptected underflow from 'very negative' valued array when 'v' is ~ 1000
+print('\n\n', softmax(N[5],N))
 
-print('\n\n\n', O)
+# very positive array
+P = np.ones(500) * v
+print('\n\n',P)
+# expected overflow from 'very positive' valued array when 'v' is ~ 1000
+print('\n\n',softmax(P[5],P))
 
-print('\n\n\n',softmax(O[2],O))
+# some constant that would typically cause over/underflow
+v = 1000
+X = np.random.randn(500) * v
+# should resolve underflow/overflow, however, may still result to final value 0
+Z = (X) - (np.amax(X))
+print('\n\n\n',Z)
+print('\n\n\n',softmax(Z[5],Z))
+# page 96
